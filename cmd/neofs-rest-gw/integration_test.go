213261d6edf7b8d1362785baddfd62f2e1a46811
Evgenii Baidakov <evgenii@nspcc.io>
2023-09-26 11:37:05 +0400

    tests: Remove force epoch ticking
    closes #94
    
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- cmd/neofs-rest-gw/integration_test.go
+++ cmd/neofs-rest-gw/integration_test.go
@@ -71,15 +71,6 @@ type dockerImage struct {
 	version string
 }
 
-var (
-	tickEpoch = []string{
-		"neo-go", "contract", "invokefunction", "--wallet-config", "/config/node-config.yaml",
-		"-a", "NfgHwwTi3wHAS8aFAN243C5vGbkYDpqLHP", "--force", "-r", "http://localhost:30333",
-		"707516630852f4179af43366917a36b9a78b93a5", "newEpoch", "int:10",
-		"--", "NfgHwwTi3wHAS8aFAN243C5vGbkYDpqLHP:Global",
-	}
-)
-
 func TestIntegration(t *testing.T) {
 	ctx := context.Background()
 	key, err := keys.NewPrivateKeyFromHex(devenvPrivateKey)
@@ -99,7 +90,7 @@ func runLocalTests(ctx context.Context, t *testing.T, key *keys.PrivateKey) {
 func runTestInContainer(rootCtx context.Context, t *testing.T, key *keys.PrivateKey) {
 	versions := []dockerImage{
 		{image: "nspccdev/neofs-aio", version: "0.37.0"},
-		{image: "nspccdev/neofs-aio", version: "latest"},
+		{image: "nspccdev/neofs-aio", version: "0.38.1"},
 	}
 
 	for _, version := range versions {
@@ -172,15 +163,6 @@ func createDockerContainer(ctx context.Context, t *testing.T, image, version str
 		Started:          true,
 	})
 	require.NoError(t, err)
-
-	// Have to wait this time. Required for new tick event processing.
-	// Should be removed after fix epochs in AIO start.
-	<-time.After(3 * time.Second)
-
-	_, _, err = aioC.Exec(ctx, tickEpoch)
-	require.NoError(t, err)
-
-	<-time.After(3 * time.Second)
 
 	return aioC
 }
