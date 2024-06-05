7fb1d55b9a21495576fac435070542270192995b
Tatiana Nesterenko <tatiana@nspcc.io>
2024-06-05 09:54:47 +0100

    log: Hide timestamp if the program is not run in TTY
    Signed-off-by: Tatiana Nesterenko <tatiana@nspcc.io>

--- cmd/neofs-rest-gw/config.go
+++ cmd/neofs-rest-gw/config.go
@@ -25,6 +25,7 @@ import (
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"golang.org/x/term"
 )
 
 const (
@@ -437,7 +438,12 @@ func newLogger(v *viper.Viper) *zap.Logger {
 	c := zap.NewProductionConfig()
 	c.Level = zap.NewAtomicLevelAt(lvl)
 	c.Encoding = "console"
-	c.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	// If the program is run in TTY, the logger adds a timestamp to its entries.
+	if term.IsTerminal(int(os.Stdout.Fd())) {
+		c.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	} else {
+		c.EncoderConfig.EncodeTime = func(_ time.Time, _ zapcore.PrimitiveArrayEncoder) {}
+	}
 
 	l, err := c.Build(
 		zap.AddStacktrace(zap.NewAtomicLevelAt(zap.FatalLevel)),
