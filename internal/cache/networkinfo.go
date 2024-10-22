0fb9b43634362f315dc169083fb0ede3723c1874
Evgenii Baidakov <evgenii@nspcc.io>
2024-10-22 17:25:20 +0400

    *: Cache network settings
    Closes #247.
    
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

---
+++ internal/cache/networkinfo.go
@@ -0,0 +1,51 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/nspcc-dev/neofs-sdk-go/client"
+	"github.com/nspcc-dev/neofs-sdk-go/netmap"
+	"github.com/nspcc-dev/neofs-sdk-go/pool"
+)
+
+type (
+	// NetworkInfo is cache wrapper for the network info.
+	NetworkInfo struct {
+		p   *pool.Pool
+		ttl time.Duration
+
+		mu         *sync.Mutex
+		validUntil time.Time
+		ni         netmap.NetworkInfo
+	}
+)
+
+func NewNetworkInfoCache(p *pool.Pool) *NetworkInfo {
+	return &NetworkInfo{
+		p:  p,
+		mu: &sync.Mutex{},
+	}
+}
+
+func (n *NetworkInfo) NetworkInfo(ctx context.Context) (netmap.NetworkInfo, error) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	if n.validUntil.After(time.Now()) {
+		return n.ni, nil
+	}
+
+	ni, err := n.p.NetworkInfo(ctx, client.PrmNetworkInfo{})
+	if err != nil {
+		return netmap.NetworkInfo{}, fmt.Errorf("get network info: %w", err)
+	}
+
+	n.ttl = time.Duration(int64(ni.EpochDuration())/2*ni.MsPerBlock()) * time.Millisecond
+	n.validUntil = time.Now().Add(n.ttl)
+	n.ni = ni
+
+	return ni, nil
+}
