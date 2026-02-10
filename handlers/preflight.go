3e4e08c2e69992961018da4552e1ccae83298363
Evgenii Baidakov <evgenii@nspcc.io>
2026-02-10 11:46:44 +0400

    *: Push CORS headers more granularly
    Closes #346.
    
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- handlers/preflight.go
+++ handlers/preflight.go
@@ -9,12 +9,19 @@ import (
 )
 
 const (
-	allOrigins   = "*"
-	allowHeaders = "X-Bearer-For-All-Users, X-Bearer-Lifetime, X-Bearer-Owner-Id, " +
-		"X-Bearer-Signature, X-Bearer-Signature-Key, Content-Type, Authorization, " +
-		"X-Attribute-Filename, X-Attribute-Filepath, X-Attributes, " +
-		"X-Neofs-Expiration-RFC3339, X-Neofs-Expiration-Timestamp, X-Neofs-Expiration-Duration, " +
-		"X-Session-Token, X-Bearer-Issuer-Id"
+	allOrigins                      = "*"
+	headerXBearerOwnerID            = "X-Bearer-Owner-Id"
+	headerXBearerLifetime           = "X-Bearer-Lifetime"
+	headerXBearerSignature          = "X-Bearer-Signature"
+	headerXBearerSignatureKey       = "X-Bearer-Signature-Key"
+	headerXBearerIssuerID           = "X-Bearer-Issuer-Id"
+	headerXNeofsBearerToken         = "NeoFS-Bearer-Token"
+	headerRange                     = "Range"
+	headerXAttributes               = "X-Attributes"
+	headerXNeofsExpirationRFC3339   = "X-Neofs-Expiration-RFC3339"
+	headerXNeofsExpirationTimestamp = "X-Neofs-Expiration-Timestamp"
+	headerXNeofsExpirationDuration  = "X-Neofs-Expiration-Duration"
+
 	allowUploadHeader = "*"
 
 	methodGet    = "GET"
@@ -25,6 +32,90 @@ const (
 
 	accessControlAllowHeadersHeader = "Access-Control-Allow-Headers"
 	accessControlAllowMethodsHeader = "Access-Control-Allow-Methods"
+
+	delimiter = ", "
+)
+
+var (
+	allowHeaders    = []string{"Content-Type", "Authorization"}
+	allowHeadersStr = strings.Join(allowHeaders, delimiter)
+
+	optionsAuth = strings.Join(
+		append(allowHeaders,
+			"X-Bearer-For-All-Users",
+			headerXBearerOwnerID,
+			headerXBearerLifetime,
+		),
+		delimiter)
+
+	optionsAuthBearer = strings.Join(
+		append(allowHeaders,
+			headerXBearerSignature,
+			headerXBearerSignatureKey,
+		),
+		delimiter)
+
+	optionsObjectsSearch = strings.Join(
+		append(allowHeaders,
+			headerXBearerSignature,
+			headerXBearerSignatureKey,
+			headerXNeofsBearerToken,
+		),
+		delimiter)
+
+	optionsObjectsGetDelete = strings.Join(
+		append(allowHeaders,
+			headerXBearerSignature,
+			headerXBearerSignatureKey,
+			headerXNeofsBearerToken,
+		),
+		delimiter)
+
+	newOptionsUploadContainerObject = strings.Join(
+		append(allowHeaders,
+			headerXBearerSignature,
+			headerXBearerSignatureKey,
+			headerXNeofsBearerToken,
+			headerXAttributes,
+			headerXNeofsExpirationRFC3339,
+			headerXNeofsExpirationTimestamp,
+			headerXNeofsExpirationDuration,
+		),
+		delimiter)
+
+	newOptionsContainerObject = strings.Join(
+		append(allowHeaders,
+			headerXBearerSignature,
+			headerXBearerSignatureKey,
+			headerXNeofsBearerToken,
+			headerRange,
+		),
+		delimiter)
+
+	newOptionsByAttribute = strings.Join(
+		append(allowHeaders,
+			headerXBearerSignature,
+			headerXBearerSignatureKey,
+			headerXNeofsBearerToken,
+			headerRange,
+		),
+		delimiter)
+
+	optionsV2SearchObjects = strings.Join(
+		append(allowHeaders,
+			headerXBearerSignature,
+			headerXBearerSignatureKey,
+			headerXNeofsBearerToken,
+		),
+		delimiter)
+
+	optionsUnsignedBearerToken = strings.Join(
+		append(allowHeaders,
+			headerXBearerIssuerID,
+			headerXBearerLifetime,
+			headerXBearerOwnerID,
+		),
+		delimiter)
 )
 
 func allowMethods(methods ...string) string {
@@ -37,7 +128,7 @@ func allowMethods(methods ...string) string {
 // OptionsAuth handler for the auth options request.
 func (a *RestAPI) OptionsAuth(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, optionsAuth)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -45,7 +136,7 @@ func (a *RestAPI) OptionsAuth(ctx echo.Context) error {
 // OptionsAuthBearer handler for the authBearer options request.
 func (a *RestAPI) OptionsAuthBearer(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, optionsAuthBearer)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -53,7 +144,7 @@ func (a *RestAPI) OptionsAuthBearer(ctx echo.Context) error {
 // OptionsBalance handler for the balance options request.
 func (a *RestAPI) OptionsBalance(ctx echo.Context, _ string) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -61,7 +152,7 @@ func (a *RestAPI) OptionsBalance(ctx echo.Context, _ string) error {
 // OptionsObjectsSearch handler for the objectsSearch options request.
 func (a *RestAPI) OptionsObjectsSearch(ctx echo.Context, _ string) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, optionsObjectsSearch)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -69,7 +160,7 @@ func (a *RestAPI) OptionsObjectsSearch(ctx echo.Context, _ string) error {
 // OptionsObjectsPut handler for the objectsPut options request.
 func (a *RestAPI) OptionsObjectsPut(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPut))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -77,7 +168,7 @@ func (a *RestAPI) OptionsObjectsPut(ctx echo.Context) error {
 // OptionsObjectsGetDelete handler for the objectsGetDelete options request.
 func (a *RestAPI) OptionsObjectsGetDelete(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.ObjectId) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, optionsObjectsGetDelete)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodDelete))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -85,7 +176,7 @@ func (a *RestAPI) OptionsObjectsGetDelete(ctx echo.Context, _ apiserver.Containe
 // OptionsContainersPutList handler for the containersPutList options request.
 func (a *RestAPI) OptionsContainersPutList(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodPut, methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -93,7 +184,7 @@ func (a *RestAPI) OptionsContainersPutList(ctx echo.Context) error {
 // OptionsContainersGetDelete handler for the containersGetDelete options request.
 func (a *RestAPI) OptionsContainersGetDelete(ctx echo.Context, _ apiserver.ContainerId) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodDelete))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -101,7 +192,7 @@ func (a *RestAPI) OptionsContainersGetDelete(ctx echo.Context, _ apiserver.Conta
 // OptionsContainersEACL handler for the containersEACL options request.
 func (a *RestAPI) OptionsContainersEACL(ctx echo.Context, _ apiserver.ContainerId) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodPut))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -109,7 +200,7 @@ func (a *RestAPI) OptionsContainersEACL(ctx echo.Context, _ apiserver.ContainerI
 // OptionsContainerObject handler for the containerObject options request.
 func (a *RestAPI) OptionsContainerObject(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.ObjectId) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodHead))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -125,7 +216,7 @@ func (a *RestAPI) OptionsUploadContainerObject(ctx echo.Context, _ apiserver.Con
 // OptionsByAttribute handler for the byAttribute options request.
 func (a *RestAPI) OptionsByAttribute(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.AttrKey, _ apiserver.AttrVal) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodHead))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -133,7 +224,7 @@ func (a *RestAPI) OptionsByAttribute(ctx echo.Context, _ apiserver.ContainerId, 
 // OptionsNetworkInfo handler for the network options request.
 func (a *RestAPI) OptionsNetworkInfo(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -141,7 +232,7 @@ func (a *RestAPI) OptionsNetworkInfo(ctx echo.Context) error {
 // NewOptionsUploadContainerObject handler for the upload object options request.
 func (a *RestAPI) NewOptionsUploadContainerObject(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.NewOptionsUploadContainerObjectParams) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowUploadHeader)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, newOptionsUploadContainerObject)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -149,7 +240,7 @@ func (a *RestAPI) NewOptionsUploadContainerObject(ctx echo.Context, _ apiserver.
 // NewOptionsContainerObject handler for the create object options request.
 func (a *RestAPI) NewOptionsContainerObject(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.ObjectId, _ apiserver.NewOptionsContainerObjectParams) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, newOptionsContainerObject)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodHead))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -157,7 +248,7 @@ func (a *RestAPI) NewOptionsContainerObject(ctx echo.Context, _ apiserver.Contai
 // NewOptionsByAttribute handler for the find by attribute options request.
 func (a *RestAPI) NewOptionsByAttribute(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.AttrKey, _ apiserver.AttrVal, _ apiserver.NewOptionsByAttributeParams) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, newOptionsByAttribute)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodHead))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -165,7 +256,7 @@ func (a *RestAPI) NewOptionsByAttribute(ctx echo.Context, _ apiserver.ContainerI
 // OptionsV2SearchObjects handler for the objectsSearch options request.
 func (a *RestAPI) OptionsV2SearchObjects(ctx echo.Context, _ string) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, optionsV2SearchObjects)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -173,7 +264,7 @@ func (a *RestAPI) OptionsV2SearchObjects(ctx echo.Context, _ string) error {
 // OptionsV2AuthSessionToken handler for the auth v2 session token options request.
 func (a *RestAPI) OptionsV2AuthSessionToken(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -181,7 +272,7 @@ func (a *RestAPI) OptionsV2AuthSessionToken(ctx echo.Context) error {
 // OptionsV2FormAuthSessionToken handler for the form v2 session token options request.
 func (a *RestAPI) OptionsV2FormAuthSessionToken(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -189,7 +280,7 @@ func (a *RestAPI) OptionsV2FormAuthSessionToken(ctx echo.Context) error {
 // OptionsGatewayMetadata handler for the gateway metadata options request.
 func (a *RestAPI) OptionsGatewayMetadata(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet))
 	return ctx.NoContent(http.StatusOK)
 }
@@ -197,7 +288,7 @@ func (a *RestAPI) OptionsGatewayMetadata(ctx echo.Context) error {
 // OptionsUnsignedBearerToken handler for the form unsigned bearer token options request.
 func (a *RestAPI) OptionsUnsignedBearerToken(ctx echo.Context) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeaders)
+	ctx.Response().Header().Set(accessControlAllowHeadersHeader, optionsUnsignedBearerToken)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
 	return ctx.NoContent(http.StatusOK)
 }
