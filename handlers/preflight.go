3eb96c1139a35df44bda417a80acd3c3e0131bdd
Evgenii Baidakov <evgenii@nspcc.io>
2026-02-10 11:51:41 +0400

    *: Remove unused
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- handlers/preflight.go
+++ handlers/preflight.go
@@ -22,8 +22,6 @@ const (
 	headerXNeofsExpirationTimestamp = "X-Neofs-Expiration-Timestamp"
 	headerXNeofsExpirationDuration  = "X-Neofs-Expiration-Duration"
 
-	allowUploadHeader = "*"
-
 	methodGet    = "GET"
 	methodHead   = "HEAD"
 	methodPost   = "POST"
@@ -157,14 +155,6 @@ func (a *RestAPI) OptionsObjectsSearch(ctx echo.Context, _ string) error {
 	return ctx.NoContent(http.StatusOK)
 }
 
-// OptionsObjectsPut handler for the objectsPut options request.
-func (a *RestAPI) OptionsObjectsPut(ctx echo.Context) error {
-	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
-	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPut))
-	return ctx.NoContent(http.StatusOK)
-}
-
 // OptionsObjectsGetDelete handler for the objectsGetDelete options request.
 func (a *RestAPI) OptionsObjectsGetDelete(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.ObjectId) error {
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
@@ -194,30 +184,6 @@ func (a *RestAPI) OptionsContainersEACL(ctx echo.Context, _ apiserver.ContainerI
 	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
 	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
 	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodPut))
-	return ctx.NoContent(http.StatusOK)
-}
-
-// OptionsContainerObject handler for the containerObject options request.
-func (a *RestAPI) OptionsContainerObject(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.ObjectId) error {
-	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
-	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodHead))
-	return ctx.NoContent(http.StatusOK)
-}
-
-// OptionsUploadContainerObject handler for the uploadContainerObject options request.
-func (a *RestAPI) OptionsUploadContainerObject(ctx echo.Context, _ apiserver.ContainerId) error {
-	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowUploadHeader)
-	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodPost))
-	return ctx.NoContent(http.StatusOK)
-}
-
-// OptionsByAttribute handler for the byAttribute options request.
-func (a *RestAPI) OptionsByAttribute(ctx echo.Context, _ apiserver.ContainerId, _ apiserver.AttrKey, _ apiserver.AttrVal) error {
-	ctx.Response().Header().Set(accessControlAllowOriginHeader, allOrigins)
-	ctx.Response().Header().Set(accessControlAllowHeadersHeader, allowHeadersStr)
-	ctx.Response().Header().Set(accessControlAllowMethodsHeader, allowMethods(methodGet, methodHead))
 	return ctx.NoContent(http.StatusOK)
 }
 
